bbaa18b867478eda06a64155976549d3e0283485
Foxyanski <foxyanski@gmail.com>
2023-03-29 18:38:53 +0300

    dynamic options from json

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,95 +1,25 @@
 <template>
   <div>
-    <header>
-      <h4>Question 1/3</h4>
-      <div class="bar">
-        <div class="completion"></div>
-      </div>
-    </header>
+    <QuizHeader />
     <div>
-      <div class="question-container">
-        <h1 class="question">What is the chemical value of table salt?</h1>
-      </div>
-      <div class="answers-container">
-        <div class="option">
-          <p class="option-label">A</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-        <div class="option">
-          <p class="option-label">B</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-        <div class="option">
-          <p class="option-label">C</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-        <div class="option">
-          <p class="option-label">D</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-      </div>
+      <Question :question="quiz.questions[currentQuestionIndex]" />
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Question from '../components/Question.vue'
+import QuizHeader from '../components/QuizHeader.vue'
+import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+import quizes from '../data/data.json'
 
-<style lang="scss" scoped>
-header {
-  margin-top: 20px;
-}
-h4 {
-  font-size: 30px;
-}
+const route = useRoute()
+const quizId = parseInt(route.params.id)
 
-.bar {
-  width: 300px;
-  height: 50px;
-  border: 3px solid bisque;
-}
+const currentQuestionIndex = ref(0)
 
-.completion {
-  height: 100%;
-  width: 0%;
-  background-color: bisque;
-}
+const quiz = quizes.find((q) => q.id === quizId)
+</script>
 
-.question-container {
-  margin-top: 20px;
-  .question {
-    font-size: 40px;
-    margin-bottom: 20px;
-  }
-}
-
-.option {
-  display: flex;
-  margin-bottom: 20px;
-  cursor: pointer;
-
-  .option-label {
-    background-color: bisque;
-    width: 60px;
-    height: 60px;
-    font-size: 30px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .option-value {
-    background-color: aliceblue;
-    width: 100%;
-    font-size: 30px;
-    padding: 0 20px;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
